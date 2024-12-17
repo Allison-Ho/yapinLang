a55667fa51a6fb4f4ab8ab7dbb696644258feeac
Allison-Ho <qnho@uw.edu>
2024-12-16 21:46:44 -0800

    add styling and placeholder text to chat window

--- client/src/Components/Chatbox/Chatbox.jsx
+++ client/src/Components/Chatbox/Chatbox.jsx
@@ -1,15 +1,44 @@
 import React from "react";
+import './Chatbox.css';
+import defaultUserImg from "../../imgs/default-user.jpg";
+import defaultBotImg from "../../imgs/default-bot.webp";
 
 function Chatbox() {
   return(
     <div id="chat-box">
-      <section className="user-buble">
-        <div className="content">
-          <p>Hi, I would like to order an iced mocha with whipped cream please</p>
+      <div className="user">
+        <div className="chat-bubble user-side">
+          <p className="time">8:30 PM</p>
+          <div className="content">
+            <p>Hi, I would like to order an iced mocha with whipped cream please</p>
+          </div>
         </div>
-        <p className="ci">Confidence rating: 80%</p>
-      </section>
-      <p className="time">8:30 PM</p>
+        <img src={defaultUserImg} alt="kermit drinking tea"></img>
+      </div>
+
+      <div className="bot">
+        <img src={defaultBotImg} alt="the wild robot"></img>
+        <div>
+          <div className="chat-bubble bot-side">
+            <div className="content">
+              <p>Welche Größe wünschen Sie?</p>
+            </div>
+            <p className="time">8:30 PM</p>
+          </div>
+
+          <p className="ci">Confidence rating: 80%</p>
+        </div>
+      </div>
+
+      <div className="user">
+        <div className="chat-bubble user-side">
+          <p className="time">8:31 PM</p>
+          <div className="content">
+            <p>I would like 8oz</p>
+          </div>
+        </div>
+        <img src={defaultUserImg} alt="kermit drinking tea"></img>
+      </div>
     </div>
   )
 }
